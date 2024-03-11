b78d8991710e299e20416e83e8cbf8095dad6b5f
gloriacho <marezone@naver.com>
2024-03-11 16:49:18 +0900

    class연습1

---
+++ class1.py
@@ -0,0 +1,15 @@
+# class1.py
+
+#defintion of class
+class Person:
+    def __init__(self) -> None:
+        self.name = "default name"
+
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+p1 = Person()
+p1.print()
+
+p1.name = "EJ"
+p1.print()
