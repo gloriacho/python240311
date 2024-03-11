00afc63ffade578970b771efd46458408cc2139d
gloriacho <marezone@naver.com>
2024-03-11 14:49:57 +0900

    함수연습2

--- DemoFunction.py
+++ DemoFunction.py
@@ -7,3 +7,29 @@ def setValue(newValue):
 
 #호출
 print(setValue(5))
+
+#
+def swap(x,y):
+    return y,x
+
+print(swap(3,4))
+
+x = 10
+def func(a):
+    return a+x
+
+print(func(1))
+
+def func2(a):
+    x = 5
+    return a+x
+
+print(func2(1))
+
+#기본값 셋팅
+def times(a=10, b=20):
+    return a*b
+
+print(times())
+print(times(5))
+print(times(5,6))
